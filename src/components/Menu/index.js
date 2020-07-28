45fc96bfea8e5868e1af51f3c4032d313c7b24d8
unknown <rafaelcavic@hotmail.com>
2020-07-28 16:22:41 -0300

    Criacao de cadastro categoria e correcao carousel

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Logo from '../../assets/img/StrikeFlix.png'
 import './Menu.css';
 import Button from '../Button';
+import {Link} from 'react-router-dom'
 
 function Menu() {
     return (
         <nav className='Menu'>
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt='StrikeFlix'/>
-            </a>
+            </Link>
 
-            <Button as="a" className='ButtonLink' href="/">
+            <Button as={Link} className='ButtonLink' to="/cadastro/video">
                 Novo Vídeo
             </Button>
         </nav>
